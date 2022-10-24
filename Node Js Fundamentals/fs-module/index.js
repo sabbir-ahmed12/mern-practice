6b2b431b855255586f62f3751965f8a04d9ba4e7
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-10-24 19:20:26 +0600

    File deleted(async) using fs module.

--- Node Js Fundamentals/fs-module/index.js
+++ Node Js Fundamentals/fs-module/index.js
@@ -4,16 +4,18 @@ var http = require("http");
 var server = http.createServer((req, res) => {
   if ((req.url = "/")) {
     // Async
-    let error = fs.renameSync("demoSync.txt", "demoSyncNew.txt");
-    if (error) {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write("File rename failed.");
-      res.end();
-    } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write("File rename successful.");
-      res.end();
-    }
+
+    fs.unlink("demoNew.txt", (error) => {
+      if (error) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("File unlink failed.");
+        res.end();
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("File unlink successful.");
+        res.end();
+      }
+    });
   }
 });
 
