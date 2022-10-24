c7247bd9f89cd013769811adb1ac8ce7e78b08ca
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-10-24 17:11:50 +0600

    Completed reading(sync) file using fs module.

--- Node Js Fundamentals/fs-module/index.js
+++ Node Js Fundamentals/fs-module/index.js
@@ -4,11 +4,17 @@ var http = require("http");
 var server = http.createServer((req, res) => {
   if ((req.url = "/")) {
     // Async
-    fs.readFile("home.html", (error, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
+    // fs.readFile("home.html", (error, data) => {
+    //   res.writeHead(200, { "Content-Type": "text/html" });
+    //   res.write(data);
+    //   res.end();
+    // });
+
+    // Sync
+    let myData = fs.readFileSync("home.html");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(myData);
+    res.end();
   }
 });
 
