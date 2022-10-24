00b02c0db297b2ac9307cc743a21cc52be9caf17
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-10-24 17:19:20 +0600

    Completed writing(async) file using fs module.

--- Node Js Fundamentals/fs-module/index.js
+++ Node Js Fundamentals/fs-module/index.js
@@ -4,17 +4,17 @@ var http = require("http");
 var server = http.createServer((req, res) => {
   if ((req.url = "/")) {
     // Async
-    // fs.readFile("home.html", (error, data) => {
-    //   res.writeHead(200, { "Content-Type": "text/html" });
-    //   res.write(data);
-    //   res.end();
-    // });
-
-    // Sync
-    let myData = fs.readFileSync("home.html");
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(myData);
-    res.end();
+    fs.writeFile("demo.txt", "Hello World", (error) => {
+      if (error) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("File Write Fail");
+        res.end();
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("File Write Success");
+        res.end();
+      }
+    });
   }
 });
 
