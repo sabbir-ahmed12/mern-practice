f528765d936f9be05a4efec70d9eeb8b53fbb64e
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-10-25 12:22:30 +0600

    Inserted data in mongodb using node.

--- Node Js Fundamentals/node-with-mongodb/index.js
+++ Node Js Fundamentals/node-with-mongodb/index.js
@@ -3,10 +3,25 @@ var mongoClient = require("mongodb").MongoClient;
 var url =
   "mongodb+srv://sabbir:1608012@cluster0.qbqrz68.mongodb.net/?retryWrites=true&w=majority";
 
-mongoClient.connect(url, (error) => {
+mongoClient.connect(url, (error, myMongoClient) => {
   if (error) {
     console.log("Connection failed.");
   } else {
     console.log("Connection successful.");
+    InsertData(myMongoClient);
   }
 });
+
+function InsertData(myMongoClient) {
+  let myDatabase = myMongoClient.db("school");
+  let myCollection = myDatabase.collection("students");
+
+  let data = { name: "Sabbir", roll: "12", dept: "ETE", city: "Cumilla" };
+  myCollection.insertOne(data, (error) => {
+    if (error) {
+      console.log("Data insert failed.");
+    } else {
+      console.log("Data insert successful.");
+    }
+  });
+}
