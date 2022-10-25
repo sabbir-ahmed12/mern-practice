1b820e94cba45e88571dedd28977027c56755a97
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-10-25 22:58:03 +0600

    Created a JSON response for about endpoint.

--- 2.express-js-fundamentals/practice-app1/index.js
+++ 2.express-js-fundamentals/practice-app1/index.js
@@ -2,9 +2,28 @@ let express = require("express");
 
 app = express();
 
+// Simple string response
 app.get("/", (req, res) => {
   {
-    res.send("Hello Express Js");
+    res.send("Home Page");
+  }
+});
+
+// JSON response
+app.get("/about", (req, res) => {
+  {
+    const myJson = [
+      { name: "Sabbir", city: "Cumilla", dept: "ETE" },
+      { name: "YP", city: "Gopalganj", dept: "ETE" },
+      { name: "Noman", city: "Savar", dept: "ETE" },
+    ];
+    res.json(myJson);
+  }
+});
+
+app.get("/contact", (req, res) => {
+  {
+    res.send("Contact Page");
   }
 });
 
