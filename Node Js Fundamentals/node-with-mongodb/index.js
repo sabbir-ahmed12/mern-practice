04713bda1fbe9af142e2c8b48e850c8b36c20697
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-10-25 16:30:26 +0600

    Added delete and search functtions using node.

--- Node Js Fundamentals/node-with-mongodb/index.js
+++ Node Js Fundamentals/node-with-mongodb/index.js
@@ -8,21 +8,79 @@ mongoClient.connect(url, (error, myMongoClient) => {
     console.log("Connection failed.");
   } else {
     console.log("Connection successful.");
-    deleteData(myMongoClient);
+    // insertData(myMongoClient);
+    // deleteAll(myMongoClient);
+    // findOneItem(myMongoClient);
+    findAllItem(myMongoClient);
   }
 });
 
-function deleteData(myMongoClient) {
+// Function to insert data from mongodb
+// function insertData(myMongoClient) {
+//   let myDatabase = myMongoClient.db("school");
+//   let myCollection = myDatabase.collection("students");
+
+//   let data = { name: "Sabbir", roll: "12", dept: "ETE", city: "Cumilla" };
+//   myCollection.insertOne(data, (error) => {
+//     if (error) {
+//       console.log("Data insert failed.");
+//     } else {
+//       console.log("Data insert successful.");
+//     }
+//   });
+// }
+
+// Function to delete data from mongodb
+// function deleteData(myMongoClient) {
+//   let myDatabase = myMongoClient.db("school");
+//   let myCollection = myDatabase.collection("students");
+
+//   let deleteItem = { roll: "12" };
+
+//   myCollection.deleteOne(deleteItem, (error) => {
+//     if (error) {
+//       console.log("Data deletion failed.");
+//     } else {
+//       console.log("Data deletion successful.");
+//     }
+//   });
+// }
+
+// Function to delete all data from collection
+// function deleteAll(myMongoClient) {
+//   let myDatabase = myMongoClient.db("school");
+//   let myCollection = myDatabase.collection("students");
+
+//   myCollection.deleteMany((error, resultObj) => {
+//     if (error) {
+//       console.log("Deletion failed.");
+//     } else {
+//       console.log(resultObj);
+//     }
+//   });
+// }
+
+// Function to find a data from collection
+// function findOneItem(myMongoClient) {
+//   let myDatabase = myMongoClient.db("school");
+//   let myCollection = myDatabase.collection("students");
+
+//   let searchItem = { roll: "10" };
+//   myCollection.findOne(searchItem, (error, result) => {
+//     if (error) {
+//       console.log("Search operation failed.");
+//     } else {
+//       console.log(result);
+//     }
+//   });
+// }
+
+// Function to find all data from collection
+function findAllItem(myMongoClient) {
   let myDatabase = myMongoClient.db("school");
   let myCollection = myDatabase.collection("students");
 
-  let deleteItem = { roll: "12" };
-
-  myCollection.deleteOne(deleteItem, (error) => {
-    if (error) {
-      console.log("Data deletion failed.");
-    } else {
-      console.log("Data deletion successful.");
-    }
+  myCollection.find().toArray((error, result) => {
+    console.log(result);
   });
 }
