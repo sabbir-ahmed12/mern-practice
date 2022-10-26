4241c35595ed2666b171e55f12f51b53542c60ce
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-10-26 08:47:36 +0600

    Created a route to work with query string.

--- 2.express-js-fundamentals/practice-app2/index.js
+++ 2.express-js-fundamentals/practice-app2/index.js
@@ -6,6 +6,15 @@ app.get("/", (req, res) => {
   res.end("This is a simple GET request.");
 });
 
+// Working with request query
+// localhost:8000/first?firstName=Sabbir&lastName=Ahmed
+app.get("/first", (req, res) => {
+  let firstName = req.query.firstName;
+  let lastName = req.query.lastName;
+
+  res.end(firstName + " " + lastName);
+});
+
 app.listen(8000, () => {
   console.log("Server run successful.");
 });
