3682377857a47498678f2ed61daf928aff588927
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2022-10-27 12:35:22 +0600

    Added response for undefined route.

--- 3.REST-api-express/practice-api/app.js
+++ 3.REST-api-express/practice-api/app.js
@@ -4,4 +4,9 @@ const app = new express();
 
 app.use("/api/v1", router);
 
+// Undefined route handling
+app.use("*", (req, res) => {
+  res.status(404).json({ status: "failed", data: "Not found" });
+});
+
 module.exports = app;
